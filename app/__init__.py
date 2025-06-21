e5045fc147536896e5cd37d34542695010bc71f2
MiguelR222 <miguel.ruiz0652@gmail.com>
2025-06-20 18:44:17 -0600

    Feat: Add About Me, Experience, Education template w/Jinja template

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
+import json
 
 load_dotenv()
 app = Flask(__name__)
@@ -9,3 +10,13 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+@app.route('/about')
+def about():
+    json_path_edu = os.path.join(app.root_path,"data", "education.json")
+    json_path_exp = os.path.join(app.root_path,"data", "experience.json")
+    with open(json_path_exp) as f:
+        experience = json.load(f)
+    with open(json_path_edu) as f:
+        education = json.load(f)
+    return render_template('about.html', title="About", url=os.getenv("URL"), education=education, experience=experience)
