0eb92e7fe879fc59f1acb9f749ab812e42609b92
MiguelR222 <miguel.ruiz0652@gmail.com>
2025-07-27 00:47:55 -0600

    Feat: Add docker to redeploy file

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -4,9 +4,6 @@ cd pe-portfolio
 git fetch
 git reset origin/main --hard
 
-python -m venv python3-virtualenv
-source python3-virtualenv/bin/activate
+docker compose -f docker-compose.prod.yml down
 
-pip install -r requirements.txt
-
-systemctl restart myportfolio
+docker compose -f docker-compose.prod.yml up -d --build
